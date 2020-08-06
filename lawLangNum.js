0c72f81450f266ab200dfe0da1d240dc4d2fbc02
LawrenzoDM <lawrenzodelmc@outlook.com>
2020-08-06 14:35:10 +0200

    adding coersion and statement

---
+++ lawLangNum.js
@@ -0,0 +1,19 @@
+// 1. concatenation
+
+let sentence = "i became a professional programmer ";
+let period = 12;
+let timeNum = " months";
+
+console.log(sentence + period + timeNum); // i became a professional programmer 12 months.
+
+// 2. converting
+
+let bigNum = 1005;
+let coersedNum = String(bigNum); 
+console.log(typeof coersedNum);  // string
+
+let wordVar = "10.05";
+let coersedString = Number(wordVar);
+console.log(typeof coersedString);  //Number
+
+// 3. Remain there
